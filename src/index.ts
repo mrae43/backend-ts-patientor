0f02da9cdac25eebb1c72cfd80ae8bf08ff7df1d
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-16 19:45:23 +0800

    feat: add new routes for diaries & update the url path with mock data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+
+import diaryRouter from './routes/diaries.js';
+
 const app = express();
 app.use(express.json());
 app.use(
@@ -10,10 +13,12 @@ app.use(
 
 const PORT = 3000;
 
-app.get('/api/ping', (_req, res) => {
+app.get('/ping', (_req, res) => {
 	console.log('someone pinged here');
 	res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
