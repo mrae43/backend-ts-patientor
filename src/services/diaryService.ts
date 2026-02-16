ecaba0bd6e9bf90b5e271ab2fbe71cf6ed219594
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-16 21:14:15 +0800

    fix: import issues & datas's file type

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -1,7 +1,9 @@
-import diaryData from '../../data/entries.json';
+import diaries from '../data/entries';
 
-const getEntries = () => {
-	return diaryData;
+import { DiaryEntry } from '../types';
+
+const getEntries = (): DiaryEntry[] => {
+	return diaries;
 };
 
 const addDiary = () => {
