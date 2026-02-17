3b5cd88ab48c269bc452e5f239668fdaf6cf4edd
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-17 22:33:25 +0800

    refactor: adjust the utils file using zod object schema

--- src/utils/utilsDiary.ts
+++ src/utils/utilsDiary.ts
@@ -1,28 +1,15 @@
 import z from 'zod';
 import { NewDiaryEntry, Visibility, Weather } from '../types';
 
+const newEntrySchema = z.object({
+	weather: z.enum(Weather),
+	visibility: z.enum(Visibility),
+	date: z.iso.date(),
+	comment: z.string(),
+});
+
 const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
-	if (!object || typeof object !== 'object') {
-		throw new Error('Incorrect or missing data');
-	}
-
-	if (
-		'comment' in object &&
-		'date' in object &&
-		'weather' in object &&
-		'visibility' in object
-	) {
-		const newEntry: NewDiaryEntry = {
-			weather: z.enum(Weather).parse(object.weather),
-			visibility: z.enum(Visibility).parse(object.visibility),
-			date: z.iso.date().parse(object.date),
-			comment: z.string().parse(object.comment),
-		};
-
-		return newEntry;
-	}
-
-	throw new Error('Incorrect data: some fields are missing');
+	return newEntrySchema.parse(object);
 };
 
 export default toNewDiaryEntry;
