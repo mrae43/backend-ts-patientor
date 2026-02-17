8710120174a0f460982941f6c5155916b6e53f4f
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-17 12:17:44 +0800

    feat: extend route to for the findById service for diary

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -4,6 +4,16 @@ import { NonSensitiveEntries } from '../types';
 
 const router: express.Router = express.Router();
 
+router.get('/:id', (req, res) => {
+	const diary = diaryService.findById(Number(req.params.id));
+
+	if (diary) {
+		res.send(diary);
+	} else {
+		res.sendStatus(404);
+	}
+});
+
 router.get('/', (_req, res: express.Response<NonSensitiveEntries[]>) => {
 	res.send(diaryService.getNonSensitiveEntries());
 });
