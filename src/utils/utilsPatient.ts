a1e9811e29ccf0380418670986449e282080ffa6
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-17 17:35:34 +0800

    feat: add safe parsing function for name & dateOfBirth

--- src/utils/utilsPatient.ts
+++ src/utils/utilsPatient.ts
@@ -1,8 +1,46 @@
 import { PatientsLog } from '../types';
 
+const isString = (text: unknown): text is string => {
+	return typeof text === 'string';
+};
+
+const parseName = (name: unknown): string => {
+	if (!name || !isString(name)) {
+		throw new Error('Incorrect or missing name');
+	}
+
+	return name;
+};
+
+const isDate = (date: string): boolean => {
+	return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+	if (!date || !isString(date) || !isDate(date)) {
+		throw new Error('Incorrect or missing date. ' + date);
+	}
+	return date;
+};
+
 const toNewPatientLog = (object: unknown): PatientsLog => {
 	if (!object || typeof object !== 'object') {
 		throw new Error('Incorrect or missing data');
+	}
+
+	if (
+		'name' in object &&
+		'dateOfBirth' in object &&
+		'ssn' in object &&
+		'gender' in object &&
+		'occupation' in object
+	) {
+		const newEntryLog: PatientsLog = {
+			name: parseName(object.name),
+			dateOfBirth: parseDate(object.dateOfBirth),
+		};
+
+		return newEntryLog;
 	}
 
 	throw new Error('Incorrect data: some fields are missing');
